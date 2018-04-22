ebbfe12677362cb945e63058c4bdc2a6d65aa5ba
Peter Cipov <peter.cipov@pricefx.eu>
2018-04-22 18:04:20 +0200

    implemented simple docker image for synapse based on debian package and using slim stretch debian base image

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # synapse-docker
-docker for synapse, pure, no other addiotions
+docker for synapse, pure, no other additions
+
+## Generate configuration
+```
+docker run -it --rm -v /config/folder:/var/matrix/conf -e MATRIX_GENERATE=true -e MATRIX_SERVER_NAME=example.com petercipov/synapse
+```
+
+## Run image
+docker run -v /config/folder:/var/matrix/conf petercipov/synapse
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+if [ $MATRIX_GENERATE = "false"] ; then
+  echo "RUNNING INSTANCE"  
+  python2.7 -m synapse.app.homeserver --config-path /var/matrix/conf/homeserver.yaml
+else
+  echo "GENERATTING CONFIG TO /var/matrix/conf"
+  python2.7 -m synapse.app.homeserver \
+    --generate-config \
+    --report-stats $MATRIX_SEND_STATS \
+    --generate-keys \
+    -H $MATRIX_SERVER_NAME \
+    --config-path /var/matrix/conf/homeserver.yaml
+fi
